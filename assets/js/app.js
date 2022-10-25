4bd991672c478cea53b7da708127c3c79cd738e9
omaima <oumaimaelmachi@gmail.com>
2022-10-25 09:56:07 +0100

    n

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,35 +16,9 @@
  let modalFooter = document.getElementById("modalFooter");
  let modalForm = document.getElementById("taskForm");
 
-  displayTasks();
-// function stock(){
-// 	 newTask["title"]= title.value;
-//      newTask["description"]=description.value;
-//      newTask["date"]=date.value;
+displayTasks();
 
-//      if( statusInput.value == 1 ){
-//          newTask["status"]="To Do";
-//      }else if( statusInput.value == 2 ){
-//          newTask["status"]="In Progress";
-//      }else if ( statusInput.value == 3 ){
-//          newTask["status"]="Done";
-//      }
-
-// 	 if( priority.value == 1 ){
-// 		newTask["priority"]="Low";
-// 	}else if( priority.value == 2 ){
-// 		newTask["priority"]="Medium";
-// 	}else if ( priority.value == 3 ){
-// 		newTask["priority"]="High";
-// 	}
-
-// 	if (typeFeature.checked == true) {
-//         newTask["type"] = "Feature";
-//     }
-//     else {
-//         newTask["type"] = "Bug";
-//     }
-//  }
+//----------------------------------Display tasks----------------------------------//
 
 function displayTasks(){
  let TODO = document.getElementById("to-do-tasks");
@@ -55,17 +29,19 @@ function displayTasks(){
 	TODO.innerHTML=" ";
 	PROGRESS.innerHTML=" ";
 	DONE.innerHTML=" ";
-let todoco=0;
-let iprco=0;
-let donco=0;
+ let todoco=0;
+ let iprco=0;
+ let donco=0;
 
-for(let i=0;i<tasks.length;i++){
+for(let i=0;i<tasks.length;i++)
+{
 	if(tasks[i].description.length>50){
 		shortdes = tasks[i].description.substring(0,50);
-	}else{ shortdes = tasks[i].description }
+	}   else{ shortdes = tasks[i].description }
+
 
 	if(tasks[i].status == "To Do"){
-		todoco++;
+	 todoco++;
      TODO.innerHTML += `
        <button class="card-body btn btn-white rounded-0 border-0 border-bottom p-2 d-flex">
 								<div class="px-3 py-2 fa-lg">
@@ -92,6 +68,7 @@ for(let i=0;i<tasks.length;i++){
 		</button>
      `;
     }
+
     if(tasks[i].status == "In Progress"){
 		iprco++;
         PROGRESS.innerHTML += `
@@ -106,8 +83,8 @@ for(let i=0;i<tasks.length;i++){
 										<div class="fw-bold" title="${tasks[i].description}">${shortdes}</div>
 									</div>
 									<div class="">
-										<span class="btn btn-primary px-2 py-1 border-0 ">${tasks[i].priority}</span>
-										<span class="btn btn-primary px-2 py-1 bg-gray bg-opacity-25 border-0 text-black ">${tasks[i].type}</span>
+										<span class="btn btn-primary rounded-pill px-2 py-1 border-0 ">${tasks[i].priority}</span>
+										<span class="btn btn-primary rounded-pill px-2 py-1 bg-gray bg-opacity-25 border-0 text-black ">${tasks[i].type}</span>
 									</div>
 								</div>
 								<div class="justify-content-end align-self-end  position-absolute end-0 mx-5 ">
@@ -120,6 +97,7 @@ for(let i=0;i<tasks.length;i++){
 
         `
     }
+
     if(tasks[i].status == "Done"){
 		donco++;
         DONE.innerHTML += `
@@ -135,8 +113,8 @@ for(let i=0;i<tasks.length;i++){
 									</div>
 
 									<div class="">
-										<span class="btn btn-primary px-2 py-1 border-0">${tasks[i].priority}</span>
-										<span class="btn btn-primary px-2 py-1 bg-gray bg-opacity-25 border-0 text-black">${tasks[i].type}</span>
+										<span class="btn btn-primary rounded-pill px-2 py-1 border-0">${tasks[i].priority}</span>
+										<span class="btn btn-primary rounded-pill px-2 py-1 bg-gray bg-opacity-25 border-0 text-black">${tasks[i].type}</span>
 									</div>
 								</div>
 							<div class="justify-content-end align-self-end  position-absolute end-0 mx-5 ">
@@ -157,18 +135,12 @@ inPCount.innerHTML = iprco;
 doneCount.innerHTML = donco;
 }
 
-
-function createTask() {
-    // initialiser task form
-    // Afficher le boutton save
-    // Ouvrir modal form
-}
+//----------------------------------Save task----------------------------------//
 
 function saveTask() {
-    // Recuperer task attributes a partir les champs input
-    // Créez task object
+    
 	document.getElementById("update").style.display="none";
-	 newTask = {
+	newTask = {
         'title'         :   title.value,
         'type'          :   '',
         'priority'      :   priority.value,
@@ -183,68 +155,38 @@ function saveTask() {
 		        newTask.type = "Bug";
 		    }
 		 
-    // Ajoutez object au Array
 	tasks.push(newTask);
-    // refresh tasks
     displayTasks();
+	reloadTasks();
 	
 }
 
-// function editTask(index) {
-//     // Initialisez task form
-// 	const task = {
-//         'title'         :   title.value,
-//         'type'          :   '',
-//         'priority'      :   priority.value,
-//         'status'        :   statusInput.value,
-//         'date'          :   date.value,
-//         'description'   :   description.value,
-// 	}
-// 	if (typeFeature.checked == true) {
-// 	           	task.type = "Feature";
-// 		    }
-// 		    else {
-// 		        task.type = "Bug";
-// 		    }
-//     // Affichez updates
-//     tasks[index] = task;
-//     // Delete Button
-// 	// Définir l’index en entrée cachée pour l’utiliser en Update et Delete
-//     // Definir FORM INPUTS
-//     // Ouvrir Modal form
-// 	displayTasks();
-// }
-// console.log(tasks);
+//----------------------------------Update task----------------------------------//
 
 function updateTask(index) {
-    // GET TASK ATTRIBUTES FROM INPUTS
-
-    // Créez task object
-
-    // Remplacer ancienne task par nouvelle task
-
-    // Fermer Modal form
-
+   
 	document.getElementById("save").style.display="none";
 	document.getElementById("update").style.display="block";
 
-    // Refresh tasks
 	let indexx = index-1;
+
 	title.value = tasks[indexx].title;
     description.value = tasks[indexx].description;
     date.value = tasks[indexx].date;
+	priority.value = tasks[indexx].priority;
+    statusInput.value = tasks[indexx].status;
+
     if (tasks[indexx].type == "Feature") {
         typeFeature.checked = true;
     }
     else {
         typeBug.checked = true;
     }
-    priority.value = tasks[indexx].priority;
-    statusInput.value = tasks[indexx].status;
+	
        
 	document.getElementById("update").onclick=()=>{
-		// Initialisez task form
-		 task = {
+
+		task = {
 			'title'         :   title.value,
 			'type'          :   '',
 			'priority'      :   priority.value,
@@ -258,41 +200,28 @@ function updateTask(index) {
 				else {
 					task.type = "Bug";
 				}
-		// Affichez updates
+
 		tasks[index-1] = task;
-		// Delete Button
-		// Définir l’index en entrée cachée pour l’utiliser en Update et Delete
-		// Definir FORM INPUTS
-		
-	document.getElementById("save").style.display="block";
-	document.getElementById("update").style.display="none";
-		// Ouvrir Modal form
+	
+		document.getElementById("update").style.display="none";
+	    document.getElementById("save").style.display="block";
+
 		displayTasks();
 		
 	}
 }
+
+//----------------------------------Delete task----------------------------------//
+
 function deleteTask(index){
-    // Get index of task in the array
-    // Remove task from array by index splice function
-    // close modal form
-    // refresh tasks
+    
 	alert("do you really want to delete this task ?");
 	tasks.splice (index-1, 1);
     displayTasks();
 
 } 
 
-function initTaskForm() {
-    // Clear task form from data
-
-    // Hide all action buttons
-}
-
-// function reloadTasks() {
-//     // Remove tasks elements
-
-//     // Set Task count
-// }
+//----------------------------------Reload Modal form----------------------------------//
 
 function reloadTasks() {
     taskForm.reset();
